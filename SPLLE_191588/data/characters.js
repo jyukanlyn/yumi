4016ca54d0c781b6d956c16478af021125b98677
jyukanlyn <jyukanlyn@gmail.com>
2026-01-15 00:34:36 +1030

    char

--- SPLLE_191588/data/characters.js
+++ SPLLE_191588/data/characters.js
@@ -2,13 +2,13 @@ export const characters = {
   "灰野　雪之丞": {
     side: "left",
     sprites: {
-      normal: "../assets/char/yuki.png"
+      normal: "assets/char/yuki.png"
     }
   },
   "ポポ": {
     side: "right",
     sprites: {
-      normal: "../assets/char/popo.png"
+      normal: "assets/char/popo.png"
     }
   }
 };
